40cb6ca70eace72c7711279d2cf4df762177861b
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:29:12 +0300

    Update

--- Project2/model/MerchantStock.java
+++ Project2/model/MerchantStock.java
@@ -1,4 +1,4 @@
-package com.example.ecommercewebsite.modle;
+package com.example.ecommercewebsite.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,17 +8,16 @@ import javax.validation.constraints.*;
 @AllArgsConstructor @Data
 public class MerchantStock {
 
-    @NotBlank(message = "Id is required")
-    @Size(min = 3, message = "Have to be 3 character long")
-    private String id;
-    @NotBlank(message = "ProductId is required")
-    @Size(min = 3, message = "Have to be 3 character long")
-    private String productid;
-    @NotBlank(message = "MerchantId is required")
-    @Size(min = 3, message = "Have to be 3 length long")
-    private String merchantid;
-    @NotBlank(message = "Stock is required")
-    @Min(value = 10, message = "have to be more than 10 at start")
+    @NotBlank(message = "id can't be empty")
+    @Size(min = 3,message = "id has to be 3 char at least")
+    private String ID;
+    @NotBlank(message = "product id can't be empty")
+    @Size( min = 3,message = "product id has to be 3 char at least")
+    private String productId;
+    @NotEmpty(message = "merchant id can't be empty")
+    @Size( min = 3,message = "merchant id has to be 3 char at least")
+    private String merchantId;
+    @NotNull(message = "stock can't be empty")
+    @Min( value = 11,message = "stock has to be more than 10")
     private Integer stock;
-
 }
