57f79189d2f289879b1ae113bf5e0aa219f6187a
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:08:40 +0300

    Update

--- Project2/Controller/MerchantController.java
+++ Project2/Controller/MerchantController.java
@@ -1,8 +1,8 @@
-package com.example.ecommercewebsite.controller;
+package com.example.ecommercewebsite.Controller;
 
-import com.example.ecommercewebsite.modle.Api;
-import com.example.ecommercewebsite.modle.Merchant;
-import com.example.ecommercewebsite.service.MerchantService;
+import com.example.ecommercewebsite2.Model.Api;
+import com.example.ecommercewebsite2.Model.Merchant;
+import com.example.ecommercewebsite2.Service.MerchantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,51 +10,54 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @RestController
-@RequestMapping("api/v1/merchant")
+@RequestMapping("api/v1/merchants")
 @RequiredArgsConstructor
 public class MerchantController {
 
     private final MerchantService merchantService;
-
     @GetMapping
-    public ResponseEntity<ArrayList<Merchant>> getMerchants(){
+    public ResponseEntity getMerchants(){
         return ResponseEntity.status(HttpStatus.OK).body(merchantService.getMerchants());
     }
 
     @PostMapping
-    public ResponseEntity<Api> addMerchants(@RequestBody @Valid Merchant merchant, Errors errors) {
-        if (errors.hasErrors()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(errors.getFieldError().getDefaultMessage(), 400));
+    public ResponseEntity addMerchants(@RequestBody @Valid Merchant merchant, Errors error){
+        if(error.hasFieldErrors()){
+            String message = error.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(message,400));
         }
-        boolean isAddMerchants = merchantService.addMerchants(merchant);
-        if (!isAddMerchants) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Error adding a merchant", 200));
+        Boolean isMerchantAdded=merchantService.addMerchants(merchant);
+        if(!isMerchantAdded){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Merchant not added",400));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("New merchant added", 200));
+        return ResponseEntity.status(HttpStatus.OK).body(new Api("Merchant added",200));
     }
 
-    @DeleteMapping("/{merchantID}")
-    public ResponseEntity<Api> deleteMerchants(@PathVariable String merchantID) {
-        Boolean deleteMerchants = merchantService.deleteMerchants(merchantID);
-        if (!deleteMerchants) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("merchantID doesn't exists!",400));
+    @PutMapping("/{merchantId}")
+    public ResponseEntity updateMerchants(@RequestBody @Valid Merchant merchant , @PathVariable String merchantId, Errors error){
+        if(error.hasFieldErrors()){
+            String err_msg=error.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(err_msg,400));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("merchantID deleted!",200));
+        Boolean isUpdateMerchant = merchantService.updateMerchants(merchant, merchantId);
+        if(!isUpdateMerchant){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("No updated",400));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new Api("Merchant updated",200));
+
     }
 
-    @PutMapping
-    public ResponseEntity<Api> updateMerchants(@RequestBody @Valid Merchant merchant, Errors errors) {
-        if(errors.hasErrors()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(errors.getFieldError().getDefaultMessage(),400));
-        }
-        Boolean updateMerchants = merchantService.updateMerchants(merchant);
-        if (!updateMerchants) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Advisor edited!",500));
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("merchant doesn't exists!",200));
-    }
+    @DeleteMapping("/{merchantId}")
+    public ResponseEntity deleteMerchants(@PathVariable String merchantId){
+        Boolean isMerchantDeleted=merchantService.deleteMerchants(merchantId);
+        if(!isMerchantDeleted){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("No deleted",400));
 
+        }
+        return ResponseEntity.status(201).body(new Api("Merchant deleted",201));
+
+    }
 }
+
