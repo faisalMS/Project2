a77578ea3b6e9c7e3fe2a1a55acfd734e56c23d9
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:04:24 +0300

    Update

--- Project2/Controller/CartController.java
+++ Project2/Controller/CartController.java
@@ -1,71 +1,71 @@
-package com.example.ecommercewebsite.controller;
+package com.example.ecommercewebsite.Controller;
 
-import com.example.ecommercewebsite.modle.Api;
-import com.example.ecommercewebsite.modle.Cart;
-import com.example.ecommercewebsite.service.CartService;
+import com.example.ecommercewebsite2.Model.Api;
+import com.example.ecommercewebsite2.Model.Cart;
+import com.example.ecommercewebsite2.Service.CartService;
+import com.example.ecommercewebsite2.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 
 @RestController
 @RequestMapping("api/v1/cart")
 @RequiredArgsConstructor
 public class CartController {
 
-    private final CartService cartService;
-
-    @GetMapping
-    public ResponseEntity<ArrayList<Cart>> getCarts() {
-        return ResponseEntity.status(HttpStatus.OK).body(cartService.getCarts());
-    }
 
 
-    @GetMapping("/{carId}")
-    public ResponseEntity getCart(@PathVariable String carId){
-        Cart tcar = cartService.findCart(carId);
-        if(car == null){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Invalid id",500));
+
+        private final CartService cartService;
+        @GetMapping
+        public ResponseEntity getCarts(){
+            return ResponseEntity.status(HttpStatus.OK).body(cartService.getCarts());
         }
-        return ResponseEntity.status(HttpStatus.OK).body(target_cart);
-    }
 
-    @PostMapping("/add")
-    public ResponseEntity addProduct(@RequestParam String cartID,@RequestParam String userID,@RequestParam String productID){
-        Integer productAdditionCase=cartService.addProduct(cartID,userID,productID);
-        switch (productAdditionCase){
-            case -1:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid cart id",400));
-            case 0:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid user id",400));
-            case 1:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid product id",400));
-            case 2:
-                return ResponseEntity.status(HttpStatus.OK).body(new Api("Product added",201));
-            default:
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Server Error",500));
-
+        @GetMapping("/{cartId}")
+        public ResponseEntity getCarts(@PathVariable String cartId){
+            Cart tarCart = cartService.getcartsId(cartId);
+            if(tarCart == null){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid id",500));
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(tarCart);
         }
-    }
+    
+        @PostMapping("/add")
+        public ResponseEntity addProducts(@RequestParam String cartId,@RequestParam String userId,@RequestParam String productId){
+            Integer addProduct = cartService.addProducts(cartId,userId,productId);
+            switch (addProduct){
+                case -1:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid cart id",400));
+                case 0:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid user id",400));
+                case 1:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid product id",400));
+                case 2:
+                    return ResponseEntity.status(HttpStatus.OK).body(new Api("Product added",200));
+                default:
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Server Error",500));
 
-    @PutMapping("/remove")
-    public ResponseEntity removeProduct(@RequestParam String cartId,@RequestParam String userId,@RequestParam String productId){
-        Integer productAdditionCase =c artService.removeProduct(cartId,userId,productId);
-        switch (productAdditionCase){
-            case -1:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid cart id",400));
-            case 0:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid user id",400));
-            case 1:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid user id",400));
-            case 2:
-                return ResponseEntity.status(HttpStatus.OK).body(new Api("Product removed",201));
-            default:
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Server Error",500));
-
+            }
         }
-    }
-
+        @PutMapping("/delete")
+        public ResponseEntity deleteProducts(@RequestParam String cartId,@RequestParam String userId,@RequestParam String productId){
+            Integer deleteProduct = cartService.deleteProducts(cartId,userId,productId);
+            switch (deleteProduct){
+                case -1:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid cart id",400));
+                case 0:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid user id",400));
+                case 1:
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Invalid product id",400));
+                case 2:
+                    return ResponseEntity.status(HttpStatus.OK).body(new Api("Product removed",200));
+                default:
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Server Error",500));
+            }
+        }
 }
