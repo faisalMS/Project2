41b0e986ca6ec51a6945fcbfc7fe00756b2d83e2
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:06:01 +0300

    Update CategoryController.java

--- Project2/Controller/CategoryController.java
+++ Project2/Controller/CategoryController.java
@@ -1,8 +1,8 @@
-package com.example.ecommercewebsite.controller;
+package com.example.ecommercewebsite.Controller;
 
-import com.example.ecommercewebsite.modle.Api;
-import com.example.ecommercewebsite.modle.Category;
-import com.example.ecommercewebsite.service.CategoryService;
+import com.example.ecommercewebsite2.Model.Api;
+import com.example.ecommercewebsite2.Model.Category;
+import com.example.ecommercewebsite2.Service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,49 +10,54 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @RestController
-@RequestMapping("api/v1/categories")
+@RequestMapping("api/v1/category")
 @RequiredArgsConstructor
 public class CategoryController {
-    private final CategoryService categoryService;
 
+    private final CategoryService categoryService;
+    
     @GetMapping
-    public ResponseEntity<ArrayList<Category>> getCategories(){
+    public ResponseEntity getCategories(){
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategories());
     }
-
+    
     @PostMapping
-    public ResponseEntity<Api> addCategories(@RequestBody @Valid Category category, Errors errors) {
-        if (errors.hasErrors()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(errors.getFieldError().getDefaultMessage(), 400));
+    public ResponseEntity addCategories(@RequestBody @Valid Category category, Errors error){
+        if(error.hasFieldErrors()){
+            String message = error.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(message,400));
         }
-        boolean isAddCategories = categoryService.addCategories(category);
-        if (!isAddCategories) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Error adding a category", 500));
+        Boolean isAddCategories = categoryService.addCategories(category);
+        if(!isAddCategories){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Category no added",400));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("New category added", 200));
+        return ResponseEntity.status(HttpStatus.OK).body(new Api("Category added",200));
     }
-
-    @DeleteMapping("/{categoryID}")
-    public ResponseEntity<Api> deleteCategories(@PathVariable String categoryID) {
-        Boolean deleteCategories = categoryService.deleteCategories(categoryID);
-        if (!deleteCategories) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("CategoriesID doesn't exists!",400));
+    
+    @PutMapping("/{categoryId}")
+    public ResponseEntity updateCategory(@RequestBody @Valid Category category , @PathVariable String categoryId, Errors error){
+        if(error.hasFieldErrors()){
+            String message = error.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(message,400));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("CategoriesID deleted!",200));
+        Boolean isUpdateCategory = categoryService.updateCategories(category,categoryId);
+        if(!isUpdateCategory){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Category no updated",400));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new Api("Category updated",200));
+
     }
+    
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity deleteCategory(@PathVariable String categoryId){
+        Boolean isDeletedCategory = categoryService.deleteCategories(categoryId);
+        if(!isDeletedCategory){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api("Category no deleted",400));
 
-    @PutMapping
-    public ResponseEntity<Api> updateCategories(@RequestBody @Valid Category category, Errors errors) {
-        if(errors.hasErrors()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Api(errors.getFieldError().getDefaultMessage(),400));
         }
-        Boolean updateCategories = categoryService.updateCategories(category);
-        if (!updateCategories) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Api("Advisor edited!",500));
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(new Api("advisorID doesn't exists!",200));
+        return ResponseEntity.status(HttpStatus.OK).body(new Api("Category deleted",200));
+
     }
 }
