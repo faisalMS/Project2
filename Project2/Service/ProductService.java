855ba2a9bb5150c9902c43c7d24037054c7d62cc
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:20:10 +0300

    Update

--- Project2/Service/ProductService.java
+++ Project2/Service/ProductService.java
@@ -1,64 +1,46 @@
-package com.example.ecommercewebsite.service;
+package com.example.ecommercewebsite.Service;
 
-
-import com.example.ecommercewebsite.modle.Product;
+import com.example.ecommercewebsite.Model.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+
 @Service
 public class ProductService {
 
-    private ArrayList<Product> productArrayList = new ArrayList<>();
-
-    public ArrayList<Product> getProducts(){
-        return productArrayList;
+    private ArrayList<Product> productArrayList =new ArrayList<>();
+    public Product getProducts(String productID) {
+        return productArrayList.get(getProductId(productID));
     }
 
-    public boolean addProducts(Product product){
-        return productArrayList.add(product);
-    }
-
-    public Boolean deleteProducts(String product) {
-        for (int i = 0; i < productArrayList.size(); i++) {
-            if (productArrayList.get(i).getId().equals(product)) {
-                productArrayList.remove(i);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public Boolean updateProducts(Product product) {
-        for (int i = 0; i < productArrayList.size(); i++) {
-            if (productArrayList.get(i).getId().equals(product.getId())) {
-                productArrayList.set(i,product);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public Product getProduct(String productID) {
-        return productArrayList.get(findProductIdx(productID));
-    }
-    public Product productsId(String productid){
-        for( Product product : productArrayList){
-            if(product.equals(productid)){
-                return product;
-            }
-        }
-        return null;
-    }
-
-
-
-    public Integer findProductIdx(String id){
-        for (int i = 0; i <productArrayList.size() ; i++) {
-            if(productArrayList.get(i).getId().equals(id)){
+    public Integer getProductId(String id){
+        for (int i = 0; i < productArrayList.size() ; i++) {
+            if(productArrayList.get(i).getID().equals(id)){
                 return i;
             }
         }
         return null;
     }
-
+    public ArrayList<Product> getProducts(){
+        return productArrayList;
+    }
+    public Boolean addProducts(Product product){
+        return productArrayList.add(product);
+    }
+    public Boolean updateProducts(Product product,String id){
+        Integer tarProductId = getProductId(id);
+        if(tarProductId == null){
+            return false;
+        }
+        productArrayList.set(tarProductId,product);
+        return true;
+    }
+    public Boolean deleteProducts(String id){
+        Integer tarProductId = getProductId(id);
+        if(tarProductId == null){
+            return false;
+        }
+        productArrayList.remove(tarProductId);
+        return true;
+    }
 }
