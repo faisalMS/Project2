8523af32b921052e2af16c04dabb9551f0a5383f
Faisal Alsofyani <83320125+faisalMS@users.noreply.github.com>
2022-05-31 02:24:53 +0300

    Update

--- Project2/Service/UserService.java
+++ Project2/Service/UserService.java
@@ -1,6 +1,7 @@
-package com.example.ecommercewebsite.service;
+package com.example.ecommercewebsite.Service;
 
-import com.example.ecommercewebsite.modle.*;
+
+import com.example.ecommercewebsite.Model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,76 +11,105 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 public class UserService {
 
-    private ArrayList<User> userList = new ArrayList<>();
-    private final ProductService productService;
 
-
+    private ArrayList<User> users = new ArrayList<>();
+    
     private final MerchantStockService merchantStockService;
+    private final ProductService productService;
     private final PurchaseHistoryService purchaseHistoryService;
-
-
-    public ArrayList<User> getUser(){
-        return userList;
+    
+    
+    public User getUsers(String userId) {
+        return users.get(getUserId(userId));
     }
-    public boolean addUsers(User user) {
-        return userList.add(user);
+    public ArrayList<User> getUsers(){
+        return users;
     }
-
-    public Boolean deleteUsers(String user) {
-        for (int i = 0; i < userList.size(); i++) {
-            if (userList.get(i).getId().equals(user)) {
-                userList.remove(i);
-                return true;
-            }
+    public Boolean addUsers(User user){
+        return users.add(user);
+    }
+    public Boolean updateUsers(User user,String id){
+        Integer tarUserId = getUserId(id);
+        if(tarUserId == null){
+            return false;
         }
-        return false;
+        users.set(tarUserId,user);
+        return true;
     }
-
-    public Boolean updateUsers(User user) {
-        for (int i = 0; i < userList.size(); i++) {
-            if (userList.get(i).getId().equals(user.getId())) {
-                userList.set(i, user);
-                return true;
-            }
+    public Boolean deleteUsers(String id){
+        Integer tarUserId = getUserId(id);
+        if(tarUserId == null){
+            return false;
         }
-        return false;
+        users.remove(tarUserId);
+        return true;
     }
 
-    public User getUsersID(String userid) {
-        for (User user : userList) {
-            if (user.getId().equals(userid)) {
-                return user;
+    public Integer getUserId(String id){
+        for (int i = 0; i <users.size() ; i++) {
+            if(users.get(i).getId().equals(id)){
+                return i;
             }
         }
         return null;
-
     }
-
-    public Integer buyProDirWithOCart(String userId,String productId,String merchantId){
-        MerchantStock merchantStock = merchantStockService.merchantStockProductsID(merchantId);
-        if(merchantStock.getStock() < 1){
+    
+    public Integer withOutCartCart(String userId,String productId,String merchantId){
+        MerchantStock tarMerchant = merchantStockService.getMerchantStocks(merchantId);
+        if(tarMerchant.getStock()<1){
             return -1;
         }
-        User user = getUsersID(userId);
-        Product product = productService.productsId(productId);
-        if(user.getBalance()<product.getPrice()){
+        User tarUser = getUsers(userId);
+        Product tarProduct = productService.getProducts(productId);
+        if(tarUser.getBalance() < tarProduct.getPrice()){
             return 0;
         }
-        merchantStock.setStock(merchantStock.getStock() -1);
+        tarMerchant.setStock(tarMerchant.getStock() -1);
 
-        user.setBalance(user.getBalance()-product.getPrice());
-        purchaseHistoryService.addPurchaseHistory2(userId, productId);
+        tarUser.setBalance(tarUser.getBalance() - tarProduct.getPrice());
+        purchaseHistoryService.histories(userId,productId);
         return 1;
     }
 
-    public ArrayList<PurchaseHistory> AllPurchaseHistory(String userID){
-        ArrayList<PurchaseHistory> purchaseHistories = new ArrayList<>();
-        for(int i = 0; i < purchaseHistoryService.getPurchaseHistories().size();i++){
-            PurchaseHistory purchaseHistory = purchaseHistoryService.getPurchaseHistories().get(i);
-            if(purchaseHistory.getUserid().equals(userID)){
-                purchaseHistories.add(purchaseHistory);
+
+    public ArrayList<PurchaseHistory> getHistories(String userId){
+        User tarUser = getUsers(userId);
+        if(tarUser == null){
+            return null;
+        }
+        ArrayList<PurchaseHistory> userHistory = new ArrayList<>();
+        for (PurchaseHistory item : purchaseHistoryService.getPurchaseHistories()) {
+            if(item.getUserId().equals(userId)){
+                userHistory.add(item);
             }
         }
-        return purchaseHistories;
+        return userHistory;
+    }
+
+
+    public Integer WithCarts(Cart cart){
+        User user= users.get(getUserId(cart.getUserId()));
+        Integer userBalance = user.getBalance();
+        int total = 0;
+        for (Product item : cart.getProducts()) {
+            Integer merchantStock = merchantStockService.getStocks(item.getID());
+            if(merchantStock < 1){
+                return -1;
+            }
+            total += item.getPrice();
+        }
+        if(userBalance < total){
+            return 0;
+        }
+        for (Product item : cart.getProducts() {
+            for (MerchantStock merchantStock: merchantStockService.getMerchantStocks()){
+                if(merchantStock.getProductId().equals(item.getID())){
+                    merchantStock.setStock(merchantStock.getStock() -1);
+                }
+            }
+            purchaseHistoryService.histories(cart.getUserId(), item.getID());
+        }
+        user.setBalance(userBalance - total);
+        return 1;
     }
 }
